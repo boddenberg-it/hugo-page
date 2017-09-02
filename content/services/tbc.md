d426e908ae1c15973184aa4b68c58cd132f67cd9
André Boddenberg <andre@boddenberg.it>
2017-09-02 16:36:48 +0200

    Add services section and run hugo before deploy to ensure latest state

---
+++ content/services/tbc.md
@@ -0,0 +1,7 @@
+---
+title: "tbc"
+date: 2017-08-20
+description: "Following soon..."
+---
+
+tbc..
--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,15 @@ if [ $# -ne 1 ]; then
 fi
 
 webroot="/www/htdocs/$1/boddenberg.it/"
+
+echo "[INFO] invoking hugo"
+hugo
+
 echo "[INFO] Deleting current remote web content"
 ssh boddenberg.it rm -rf "${webroot}*"
+
 echo "[INFO] Uploading local web content"
 scp -r public/* boddenberg.it:"$webroot"
+
 echo
 echo "[INFO] done."
