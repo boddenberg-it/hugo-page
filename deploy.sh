38fe6d2b19bc2f30379eccac846e5af00e3643ec
André Boddenberg <andre@boddenberg.it>
2017-09-02 14:58:36 +0200

    Change deploy script to target production server

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
-hugo
-sudo rm -rf /var/www/html/*
-sudo cp -r public/* /var/www/html/
+#!/bin/sh -e
+
+if [ $# -ne 1 ]; then
+	echo "[ERROR] Please pass all.inkl user ID"; exit 1
+fi
+
+webroot="/www/htdocs/$1/boddenberg.it/"
+echo "[INFO] Deleting current remote web content"
+ssh boddenberg.it rm -rf "${webroot}*"
+echo "[INFO] Uploading local web content"
+scp -r public/* boddenberg.it:"$webroot"
+echo
+echo "[INFO] done."
