a896354cb6ebb7fb1860c4a27e786aaf37aab94c
André Boddenberg <dr.blobb@gmail.com>
2017-10-02 21:00:08 +0200

    Move HOST to .deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,30 @@
 # update minimal theme:
 #   git submodule update --remote themes/minimal
 
+# SANITY CHECK
 if [ ! -f .deploy ]; then
-	echo "[ERROR] Please provide .deploy file holding USER and DOMAIN"; exit 1
+	echo "[ERROR] Please provide .deploy file holding HOST, USER and DOMAIN";
 	exit 1
 fi
 
-# load config
-. ./.deploy
+. ./.deploy # load config
 webroot="/www/htdocs/$USER/$DOMAIN/"
 
+if [ -z ${HOST+x} ] || [ -z ${USER+x} ] || [ -z ${DOMAIN+x} ]; then
+	echo "[ERROR] Please provide HOST, User and Domain within .deploy file"
+	exit 1
+fi
+
+# ACTUAL DEPLOYMENT
 echo "[INFO] invoking hugo"
 hugo
 
 echo "[INFO] Deleting current remote web content"
-ssh boddenberg.it mv "${webroot}/usage" "/${webroot}/../usage-$DOMAIN"
-ssh boddenberg.it rm -rf "${webroot}*"
-ssh boddenberg.it mv "/${webroot}/../usage-$DOMAIN" "${webroot}/usage"
+ssh "$HOST" mv "${webroot}/usage" "/${webroot}/../usage-$DOMAIN"
+ssh "$HOST" rm -rf "${webroot}*"
+ssh "$HOST" mv "/${webroot}/../usage-$DOMAIN" "${webroot}/usage"
 
 echo "[INFO] Uploading local web content"
-scp -r public/* boddenberg.it:"$webroot"
+scp -r public/* ${HOST}:"$webroot"
 
 echo "[INFO] done."
